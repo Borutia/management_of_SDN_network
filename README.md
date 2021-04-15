7ca7a9342572f204f7944b6c8bf01b4c081f9dde
Dmitry Borutya <borutia@mail.ru>
2021-04-15 22:09:19 +0500

    fixed and added readme

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# management_of_SDN_network
+# Management_of_SDN_network
 Построения сети управления в SDN сети
 
+# Зависимости
+Для работы приложения необходима версия языка Python 3.6 и выше.
+Все библиотеки испульзуются стандартные. 
+
+# Запуск
+Программа принимает на вход путь к файлу в формате GraphML, 
+дополнительный парамерты k1 и k2. 
+
+Пример запуска программы:
+
+```
+>>> python3 ./main.py -t tests/AttMpls.graphml -k1 3 -k2 22
+Parsed arguments:
+destination_node_id = 22
+file_name = tests/AttMpls.graphml
+source_node_id = 3
+Parsed 25 nodes
+Parsed 114 edges
+Parsed graph: G(25 nodes, 114 edges)
+Written topology to file tests/AttMpls.graphml_topo.csv
+Written routes to file tests/AttMpls.graphml_routes.csv
+Visualization available at file tests/AttMpls.graphml_demo.html
+
+```
+
+Программа генерирует два csv файла и, если указаны параметры -k1 и -k2, 
+один HTML файл с визуализацией графа. 
+Прямой путь обозначается черным, резервный - красным.
+
+Для вычисления расстояния между двумя точками, заданными в координатах, 
+используется формула:
+[greate-circle distance](https://en.wikipedia.org/wiki/Great-circle_distance).
+
+# Визуализация графа
+![alt text](image/graph.png "www.topology-zoo.org, ATT North America")
--- main.py
+++ main.py
@@ -19,11 +19,9 @@ def main(args):
                 graph, i, j, dist, next_vertex
             )
             routes.append(direct_route)
-
-            if args.build_reserve:
-                routes.append(algorithms.find_reserve_route(
-                    graph, i, j, direct_route.path)
-                )
+            routes.append(algorithms.find_reserve_route(
+                graph, i, j, direct_route.path)
+            )
 
     dump.dump_routes(graph, routes, args.file_name)
 
@@ -34,12 +32,9 @@ def main(args):
         direct_route = algorithms.extract_route(
             graph, start_idx, dest_idx, dist, next_vertex
         )
-        reserve_route = None
-        if args.build_reserve:
-            reserve_route = algorithms.find_reserve_route(
-                graph, start_idx, dest_idx, direct_route.path
-            )
-
+        reserve_route = algorithms.find_reserve_route(
+            graph, start_idx, dest_idx, direct_route.path
+        )
         visualize.visualize(
             graph, args.file_name + '_demo.html', direct_route, reserve_route
         )
@@ -49,11 +44,9 @@ if __name__ == '__main__':
     arg_parse = argparse.ArgumentParser()
     arg_parse.add_argument('-t', '--file-name', required=True,
                            help='filename to parse, .gml or .graphml')
-    arg_parse.add_argument('-r', '--build-reserve', action='store_true',
-                           help='build reserve routes')
-    arg_parse.add_argument('-s', '--source-node-id',
+    arg_parse.add_argument('-k1', '--source-node-id',
                            help='source node id to visualize')
-    arg_parse.add_argument('-d', '--destination-node-id',
+    arg_parse.add_argument('-k2', '--destination-node-id',
                            help='destination node id to visualize')
     args = arg_parse.parse_args()
     print('Parsed arguments:')
